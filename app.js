be42bc2fcc6085464270337885803bcfe5f58345
Julian Gomez <julian1995ag@gmail.com>
2023-09-05 22:28:17 -0300

    avances

--- app.js
+++ app.js
@@ -8,9 +8,21 @@ const indexRouter = require ('./src/routes/index')
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+
+
 const cookie_secret = process.env.cookie_secret || '123456'
 //forma de firmar la cookie
 app.use(cookieParser(cookie_secret));
+
+const storeConfig = {path:'./session', ttl:300, retries: 0}
+app.use(session({
+    store: new FileStore(storeConfig),
+    secret:cookie_secret,
+    resave:true,
+    saveUninitialized:true
+}))
 app.use(indexRouter);
 
 
